3d00d2d58dbdfd1582fa36fbb5262e498e3919b1
sujan_aryal <sujan_aryal@dell.com>
2019-05-01 19:56:25 -0500

    App.js update

--- react/src/App.js
+++ react/src/App.js
@@ -1,73 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import Clients from './containers/Clients';
+
+// import { BrowserRouter, Route, Link } from "react-router-dom";
 
 class App extends Component {
   state = {
     clients: []
   }
 
-  componentDidMount = () => {
-    this.fetchClients();
-  }
-
-  fetchClients = async () => {
-    const response = await fetch('/api/clients');
-    const clients = await response.json();
-    this.setState({ clients: clients });
-  }
-
-  addClient = async (e) => {
-    e.preventDefault(); // Don't refresh the browser
-    await fetch('/api/clients', {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        "name": e.target.elements["name"].value,
-        "address" : e.target.elements["address"].value,
-        "phoneNumber":e.target.elements["phoneNumber"].value
-      })
-    });
-    this.fetchClients();
-  }
-
-  render = () => {
+  render() {
     return (
-      <div>
-        <h1>Clients</h1>
-        <form onSubmit={this.addClient}>
-          <label>
-            name
-            <input name="name" />
-          </label>
-          <label>
-            address
-            <input name="address" />
-          </label>
-          <label>
-            phone number
-            <input name="phoneNumber" />
-          </label>
-          <input type="submit"></input>
-        </form>
-        <table className="pure-table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Address</th>
-              <th>Phone Number</th>
-            </tr>
-          </thead>
-          <tbody>
-              {this.state.clients.map(client => <tr> <td key={client.id}>{client.id}</td> <td>{client.name}</td> <td>{client.address}</td> <td>{client.phoneNumber}</td></tr>)}
-          </tbody>
+      <main>
+        <Clients />
 
-        </table>
+      {/* <BrowserRouter>
+        <Link to="/clients/list">List</Link>
+        <br />
+        <Link to="/clients/">Clients</Link>
+      
+        <Route exact path="/clients/" component={Clients} />
+      </BrowserRouter> */}
 
-      </div>
-    )
+        
+      </main>
+    );
   }
 }
 
