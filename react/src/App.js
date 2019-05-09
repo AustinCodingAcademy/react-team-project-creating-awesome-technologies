9bdedcd2e2a669a2f4d36bfdbfbe3fcf3afc6005
catherine burns <csburns33@gmail.com>
2019-05-08 19:51:12 -0500

    Add pets

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 import React, { Component } from 'react';
 import './App.css';
 import Clients from './containers/Clients';
+=======
+import React, { Component } from "react";
+import "./App.css";
+import Clients from "./containers/Clients";
+import Header from "./containers/Header";
+import Pets from "./containers/Pets";
+>>>>>>> Stashed changes
 
 // import { BrowserRouter, Route, Link } from "react-router-dom";
 
@@ -22,7 +30,26 @@ class App extends Component {
         <Route exact path="/clients/" component={Clients} />
       </BrowserRouter> */}
 
+<<<<<<< Updated upstream
         
+=======
+        <Container>
+          <Nav className="ml-auto " navbar>
+            <NavItem>
+              <NavLink href="/clients">Clients</NavLink>
+              <NavLink href="/pets">Pets</NavLink>
+              <NavLink href="/appointments">Appointments</NavLink>
+            </NavItem>
+          </Nav>
+          <BrowserRouter>
+            <br />
+            <Route exact path="/clients/" component={Clients} />
+            {/* <Link to="/pets">pets</Link> */}
+            <br />
+            <Route exact path="/pets/" component={Pets} />
+          </BrowserRouter>
+        </Container>
+>>>>>>> Stashed changes
       </main>
     );
   }
--- react/src/components/ClientComponents/ClientList.js
+++ react/src/components/ClientComponents/ClientList.js
@@ -1,5 +1,12 @@
+<<<<<<< Updated upstream
 import React, { Component } from 'react'
 
+=======
+
+import React, { Component } from 'react'
+import AddPetForm from '../PetComponents/AddPetForm';
+import { NavLink } from 'react-router-dom'
+>>>>>>> Stashed changes
 
 
 export default class ClientList extends Component {
@@ -7,10 +14,16 @@ export default class ClientList extends Component {
     return (
       <div>
 
+<<<<<<< Updated upstream
         <ul>
           {this.props.clients.map(client => <li key={client.id}>{client.name}</li>)}
         </ul>
         
+=======
+        </Table>
+        <ul />
+
+>>>>>>> Stashed changes
       </div>
     )
   }
