89459563884c3ee535eaf103ba358920694b1a60
Patti Parnell <Patricia_Parnell@dell.com>
2019-05-15 20:13:25 -0500

    fix

--- react/src/components/PetComponents/AddPetForm.js
+++ react/src/components/PetComponents/AddPetForm.js
@@ -3,11 +3,27 @@ import React, { Component } from 'react'
 
 export default class AddPetForm extends Component {
   render() {
+    console.log("add pet form");
+
     return (
       <div>
 
+        <form name="savePet"  onSubmit={this.props.addPet}>
+          <input type="number" name="clientId"/>
 
-        
+          <label> Pet Name
+            <input name="name" />
+          </label>
+          <label> Gender
+            <input name="gender" />
+          </label>
+          <label> Fixed?
+            <input type="checkbox" name="fixed" />
+            <input type="submit" />
+
+          </label>
+          <input type="submit" />
+        </form>
       </div>
     )
   }
